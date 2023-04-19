5879ea2105ca31958c453c008b62d2cd2f6d5440
roy.vanberkum.bee <roy.vanberkum.bee@outlook.com>
2023-04-19 13:49:40 +0200

    wip

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,4 +2,18 @@ package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
+public class MainActivity extends BridgeActivity {
+
+     @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+
+        AppCenter.start(getApplication(), "eaed1aa8-2aff-4dcf-ad80-c827619590e2",
+            Analytics.class, Crashes.class);
+     }
+
+}
