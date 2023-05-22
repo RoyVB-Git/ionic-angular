04f6026c5fb467ba9c2596ac3a3ffb92aba4a8d8
RoyB <royvberkum@gmail.com>
2023-05-22 14:10:12 +0200

    Create bump.js

---
+++ gulp/bump.js
@@ -0,0 +1,14 @@
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import bump from 'gulp-bump';
+
+const paths = [
+  './version.json',
+  './package.json',
+];
+
+export default function (type) {
+  return gulp.src(paths, { base: './' })
+    .pipe(bump({ type }))
+    .pipe(gulp.dest('.'));
+}
